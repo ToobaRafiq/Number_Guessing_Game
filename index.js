f0cfd319269d5aa6e9c30255c064ebe51987dfdc
ToobaRafiq <165496515+ToobaRafiq@users.noreply.github.com>
2024-04-06 07:53:33 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+console.log("\n\tWelcome to tooba_rafiq - CLI Number_Guessing_Game\n");
+const ramdomNumber = Math.floor(Math.random() * 3 + 1);
+const anwser = await inquirer.prompt([
+    {
+        name: 'userGuessedNumber',
+        type: 'number',
+        message: 'Enter you guess number(Number limit from 3 to 1)'
+    }
+]);
+if (anwser.userGuessedNumber === ramdomNumber) {
+    console.log('Congratulation! you guess a correct number.');
+}
+else {
+    console.log("Sorry! You guess a wrong number.");
+}
